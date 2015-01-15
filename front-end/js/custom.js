899b4c08726a80be93c1909338e01967d42bdee4
ntduy <truongduyng@gmail.com>
2015-01-15 14:55:57 +0700

    first commit

--- front-end/js/custom.js
+++ front-end/js/custom.js
@@ -1,3 +1,13 @@
-jQuery(document).ready(function ($){
+$(document).ready(function () {
+	var menu = $('.menu');
+	var origOffsetY = menu.offset().top;
 
+	function scroll() {
+		if ($(window).scrollTop() >= origOffsetY) {
+			$('.menu').addClass('navbar-fixed-top');
+		} else {
+			$('.menu').removeClass('navbar-fixed-top');
+		}
+	}
+	document.onscroll = scroll;
 });
