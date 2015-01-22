a42177667e2e0ba49df61a86fbf90e1d902fc4a7
ntduy <truongduyng@gmail.com>
2015-01-22 17:10:55 +0700

    error test controller

--- demoapp/app/models/user.rb
+++ demoapp/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+	validates :name, length: {maximum: 20}, presence: true
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.+\d]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+	format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
 end
