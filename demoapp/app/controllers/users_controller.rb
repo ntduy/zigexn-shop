ee2abfb72694e64f6832d03054dc2b4df900b802
ntduy <truongduyng@gmail.com>
2015-01-23 17:03:35 +0700

    current task login

--- demoapp/app/controllers/users_controller.rb
+++ demoapp/app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+  	@users = User.all
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    # debugger
   end
 
   # GET /users/new
   def new
-    @user = User.new
+  	@user = User.new
   end
 
   # GET /users/1/edit
@@ -24,51 +25,38 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+  	@user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+  	if @user.save
+  		log_in @user
+  		flash.now[:success] = "Welcome to the Sample App!"
+  		render 'show'
+  	else
+  		flash.now[:danger] = 'danger'
+  		render 'new'
+  	end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+  	
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  	
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+    	@user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-end
+  end
--- demoapp/app/models/user.rb
+++ demoapp/app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+	before_save {save.email = email.downcase}
 	has_many :microposts
 	validates :name, length: {maximum: 20}, presence: true
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.+\d]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
 	format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+
+	has_secure_password
+	validates :password, length: { minimum: 6 }
 end
